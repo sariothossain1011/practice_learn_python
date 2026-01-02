ec88508a24972671819f050a510a8f97a3771808
Sariot Hossain <sariothossain1011@gmail.com>
2026-01-02 23:43:30 +0600

    type casting and strings

---
+++ basic-level/casting.py
@@ -0,0 +1,16 @@
+x = float(1)
+y = float(2.8)
+z = float("3")
+w = float("4.2")
+print(x)
+print(y)
+print(z)
+print(w)
+
+
+
+b = "Hello, World!"
+print(b[2:5])
+
+a = "Hello, World!"
+print(a.lower())
--- basic-level/type.py
+++ basic-level/type.py
@@ -14,3 +14,4 @@ x = y = z = "Orange"
 print(x)
 print(y)
 print(z)
+
